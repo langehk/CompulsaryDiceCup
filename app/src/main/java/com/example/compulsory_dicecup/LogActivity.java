b55a6683e5e8fe6d0afe40bad7ce5192e7a226ac
Bomholt <svenbomholt@gmail.com>
2019-02-28 15:20:28 +0100

    logActivity working!!!

--- app/src/main/java/com/example/compulsory_dicecup/LogActivity.java
+++ app/src/main/java/com/example/compulsory_dicecup/LogActivity.java
@@ -2,25 +2,28 @@ package com.example.compulsory_dicecup;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import java.util.ArrayList;
 
 public class LogActivity extends AppCompatActivity {
+    ListView diceListView;
+    ArrayList<String> test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        setUp();
         setGui();
         configureBackButton();
-        setUp();
-
     }
 
 
     private void setUp() {
-        ListView diceListView = this.findViewById(R.id.diceListView);
+        diceListView = this.findViewById(R.id.diceListView);
     }
 
     private void configureBackButton(){
@@ -35,10 +38,16 @@ public class LogActivity extends AppCompatActivity {
 
     private void setGui(){
         ArrayList<BEDiceRoll> rolls = (ArrayList<BEDiceRoll>) getIntent().getSerializableExtra("History");
-
+        test = new ArrayList<>();
         for (BEDiceRoll roll : rolls)
         {
-           ;
+           test.add(roll.getTimeStamp().toString());
         }
+
+        ListAdapter adapter =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1, test);
+
+        diceListView.setAdapter(adapter);
     }
 }
