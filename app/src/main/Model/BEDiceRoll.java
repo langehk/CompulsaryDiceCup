7c34a514423ff22d06ce88bb2c3a7d681b20a9bd
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-02-28 09:18:43 +0100

    Create BEDiceRoll.java

---
+++ app/src/main/Model/BEDiceRoll.java
@@ -0,0 +1,22 @@
+package com.example.compulsory_dicecup;
+
+import java.io.Serializable;
+
+public class BEDiceRoll implements Serializable{
+
+    private int[] m_diceRolls;
+    private int m_diceId;
+
+    public BEDiceRoll(int[] diceRolls, int id){
+        m_diceRolls = diceRolls;
+        m_diceId = id;
+    }
+
+    public int[] getDiceRolls(){
+        return m_diceRolls
+    }
+
+    public int getDiceId(){
+        return m_diceId;
+    }
+}
