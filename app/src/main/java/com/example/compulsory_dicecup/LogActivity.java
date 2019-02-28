a42056ac3ab732039c41ff38c8afd64abb995165
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2019-02-28 12:50:56 +0100

    Update LogActivity.java

--- app/src/main/java/com/example/compulsory_dicecup/LogActivity.java
+++ app/src/main/java/com/example/compulsory_dicecup/LogActivity.java
@@ -13,7 +13,7 @@ public class LogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
-
+        setGui();
         configureBackButton();
     }
 
